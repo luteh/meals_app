fe16f67c17c72ce9e262d36a1ecfce52a8caadbd
luthfan.maftuh <luthfan.maftuh@uangteman.com>
2020-01-18 18:34:19 +0700

    Add category item widget

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'package:meals_app/dummy_data.dart';
 
 ///
 /// Created by Luthfan Maftuh on 1/18/2020.
@@ -7,7 +9,7 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[],
+      children: DUMMY_CATEGORIES.map((data) => CategoryItem(title: data.title, color: data.color)).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
